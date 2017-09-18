1fa797a3f7d144cda90314b209f16cfad00d6e68
John <jhallman5@gmail.com>
2017-09-18 10:17:02 -0700

    adds css framework bulma

--- src/server.js
+++ src/server.js
@@ -11,7 +11,8 @@ require('ejs')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.static('public'))
+app.use('/bulma', express.static('node_modules/bulma/css'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
