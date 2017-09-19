38d075c101b8252722e05f5437c507d0a84b7a93
John <jhallman5@gmail.com>
2017-09-19 08:22:18 -0700

    Updates user.findbyid to include album indfo, adds reviews section to users page

--- src/models/db/queries/users.js
+++ src/models/db/queries/users.js
@@ -1,7 +1,7 @@
 const db = require('../init')
 
 const findById = (id) =>
-  db.any('SELECT *, reviews.id AS review_id FROM users LEFT OUTER JOIN reviews ON users.id = reviews.user_id WHERE users.id = $1 ',[id])
+  db.any('SELECT *, reviews.id AS review_id FROM users LEFT OUTER JOIN reviews ON users.id = reviews.user_id LEFT OUTER JOIN albums ON albums.id = reviews.album_id WHERE users.id = $1 ',[id])
 
 module.exports = {
   findById
--- src/models/users.js
+++ src/models/users.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 const User = require('./db/queries/users')
 
 const findById = (id) =>
@@ -8,9 +9,10 @@ const findById = (id) =>
         member_since: user[0].member_since,
         reviews: user.map(review => ({
           id: review.review_id,
-          album_id: review.ablum_id,
+          album_title: review.title,
+          album_id: review.album_id,
           content: review.content,
-          created_on: review.created_on
+          created_on: moment(review.created_on).format('MMM Do YYYY')
         }))
     }))
 
