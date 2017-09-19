bb4f6799d4302e5b9dd509a9cc2073c4c45417b0
John <jhallman5@gmail.com>
2017-09-19 16:42:31 -0700

    Adds a review card partial

--- src/models/users.js
+++ src/models/users.js
@@ -5,10 +5,13 @@ const findById = (id) =>
   User.findById(id)
     .then( user => ({
         id: user[0].user_id,
+        username: user[0].username,
         email: user[0].email,
-        member_since: user[0].member_since,
+        member_since: moment(user[0].member_since).format('MMM Do YYYY'),
         reviews: user.map(review => ({
           id: review.review_id,
+          user_id: review.user_id,
+          username: review.username,
           album_title: review.title,
           album_id: review.album_id,
           content: review.content,
