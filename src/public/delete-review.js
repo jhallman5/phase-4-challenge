2594740fb505f53acc369c6c624a04766e7ff55d
John <jhallman5@gmail.com>
2017-09-20 09:57:20 -0700

    updates event.target to event.currenttarget

--- src/public/delete-review.js
+++ src/public/delete-review.js
@@ -1,14 +1,17 @@
-document.querySelectorAll('.my-review').forEach(card =>
-  card.addEventListener('click', (event) => {
-    confirm("Are you sure you want to delete this review?")
-      ? deleteReview(event.target.dataset.albumid, event.target.dataset.reviewid)
-      : event.preventDefault()
-}))
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.my-review').forEach(card =>
+    card.addEventListener('click', (event) => {
+      confirm("Are you sure you want to delete this review?")
+        ? deleteReview(event.currentTarget.dataset.albumid, event.currentTarget.dataset.reviewid)
+        : event.preventDefault()
+  }))
 
-const deleteReview = (albumId, reviewId, target) => {
-  fetch(`http://localhost:3000/albums/${albumId}/reviews/${reviewId}`, {method: 'DELETE'})
-    .then(response => response.text())
-    .then( () => {
-      location.reload(true)
-    })
-}
+  const deleteReview = (albumId, reviewId) => {
+    fetch(`http://localhost:3000/albums/${albumId}/reviews/${reviewId}`, {method: 'DELETE'})
+      .then(response => response.text())
+      .then( () => {
+        location.reload(true)
+      })
+  }
+
+})
