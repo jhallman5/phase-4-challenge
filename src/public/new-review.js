474f3dc4ae09384d055e1eb3da6ee75bd4fc4e99
John <jhallman5@gmail.com>
2017-09-20 11:34:48 -0700

    Updates querySelector for new-review

--- src/public/new-review.js
+++ src/public/new-review.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  if(document.querySelector('.button')) {
-    document.querySelector('.button').addEventListener('click', (event) => {
+  if(document.querySelector('.new-review')) {
+    document.querySelector('.new-review').addEventListener('click', (event) => {
       if(document.querySelector('.textarea').value.replace(/\s/g,'').length < 1) {
         event.preventDefault()
         alert("You can't submit an empty review!")
