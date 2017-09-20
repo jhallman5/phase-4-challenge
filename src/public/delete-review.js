c23759958d25067e45597428730ce670f429b87b
John <jhallman5@gmail.com>
2017-09-20 07:42:19 -0700

    Updates front end js

--- src/public/delete-review.js
+++ src/public/delete-review.js
@@ -1,12 +1,12 @@
-document.querySelectorAll('.my-review').forEach(review => review.addEventListener('click', (event) => {
-  const data = event.target.dataset
-  confirm("Are you sure you want to delete this review?")
-    ? deleteReview(data.albumid, data.reviewid)
-    : event.preventDefault()
+document.querySelectorAll('.my-review').forEach(card =>
+  card.addEventListener('click', (event) => {
+    confirm("Are you sure you want to delete this review?")
+      ? deleteReview(event.target.dataset.albumid, event.target.dataset.reviewid)
+      : event.preventDefault()
 }))
 
 const deleteReview = (albumId, reviewId, target) => {
-  fetch(`/albums/${albumId}/reviews/${reviewId}`, {method: 'DELETE'})
+  fetch(`http://localhost:3000/albums/${albumId}/reviews/${reviewId}`, {method: 'DELETE'})
     .then(response => response.text())
     .then( () => {
       location.reload(true)
--- src/public/new-review.js
+++ src/public/new-review.js
@@ -1,6 +1,8 @@
-document.querySelector('.button').addEventListener('click', (event) => {
-  if(document.querySelector('.textarea').value.replace(/\s/g,'').length < 1) {
-    event.preventDefault()
-    alert("You can't submit an empty review!")
-  }
-})
+if(document.querySelector('.button')) {
+  document.querySelector('.button').addEventListener('click', (event) => {
+    if(document.querySelector('.textarea').value.replace(/\s/g,'').length < 1) {
+      event.preventDefault()
+      alert("You can't submit an empty review!")
+    }
+  })
+}
